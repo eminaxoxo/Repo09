a414dec8ec271e7015e9103a35b37eb3ae056e01
eminaxoxo <100778616+eminaxoxo@users.noreply.github.com>
2022-04-07 10:51:41 +0200

    Update index.js

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,16 +1,26 @@
 function logicalOrOperator(num) {
-    if (num >= 20 || num <= 30) {
-      return "Out";
+    if (num > 30 || num < 20) {
+        return "Out";
     }
     return "In";
-  }
-  console.log(logicalOrOperator(0));
-  console.log(logicalOrOperator(9));
-  console.log(logicalOrOperator(20));
-  console.log(logicalOrOperator(23));
-  console.log(logicalOrOperator(30));
-  console.log(logicalOrOperator(31));
-  console.log(logicalOrOperator(105));
-  
-  // Only change code above this line
+}
+
+console.log(logicalOrOperator(0));
+console.log(logicalOrOperator(9));
+console.log(logicalOrOperator(20));
+console.log(logicalOrOperator(23));
+console.log(logicalOrOperator(30));
+console.log(logicalOrOperator(31));
+console.log(logicalOrOperator(105));
+
+module.exports = logicalOrOperator;
+
+  logicalOrOperator(0);
+  logicalOrOperator(9);
+  logicalOrOperator(20);
+  logicalOrOperator(23);
+  logicalOrOperator(30);
+  logicalOrOperator(31);
+  logicalOrOperator(105);
+
   module.exports = logicalOrOperator;
